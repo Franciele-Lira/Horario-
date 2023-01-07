0b183cc95fa977687153319641a1ef8b92734b08
Franciele-Lira <francielelira22@gmail.com>
2023-01-07 09:51:12 -0300

    Atualização

--- script.js
+++ script.js
@@ -3,10 +3,11 @@ function carregar() {
 var msg = window.document.getElementById('msg')
 var img = window.document.getElementById('imagem')
 var data = new Date()
-var hora = data.toLocaleTimeString() //hora com minutos e segundos
-//var hora = 21
-msg.innerHTML = `Agora são ${hora} horas`
-if (hora >= 0 && hora < 12){
+var hora = data.getHours()
+var hora_msg = data.toLocaleTimeString()
+
+msg.innerHTML = `Agora são ${hora_msg.substring(0,5)} horas,`
+    if (hora >= 1 && hora < 12 ){
     //Bom Dia
     img.src = 'manha.jpg.jpg'
     document.body.style.background = '#e2cd9f'
